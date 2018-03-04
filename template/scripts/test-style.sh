45d384f8c523ff1a3e30a97d2ecdbdd3289a17d4
Ian MacLeod <ian@nevir.net>
2018-03-03 21:32:26 -0800

    Add eslint/prettier as dev deps

--- template/scripts/test-style.sh
+++ template/scripts/test-style.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
+set -e
 
+FILES=("${@}")
+if [[ "${#FILES[@]}" = "0" ]]; then
+  FILES=($(
+    find . \
+      -path './node_modules' -prune -o \
+      -not -name '\.*' \
+      -type f \( \
+        -name '*.mjs' -o \
+        -name '*.js' -o \
+        -name '*.jsx' -o \
+        -name '*.ts' -o \
+        -name '*.tsx' \
+      \) \
+      -print
+  ))
+fi
+
+./node_modules/.bin/eslint "${FILES[@]}"
