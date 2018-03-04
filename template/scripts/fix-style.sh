eccb303d23f97d895821199e64cad21172649efd
Ian MacLeod <ian@nevir.net>
2018-03-03 21:52:10 -0800

    Fix style script

---
+++ template/scripts/fix-style.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+FILES=("${@}")
+if [[ "${#FILES[@]}" = "0" ]]; then
+  FILES=($(
+    find . \
+      -path './node_modules' -prune -o \
+      -not -name '\.*' \
+      -type f \( \
+        -name '*.mjs' -o \
+        -name '*.js' -o \
+        -name '*.jsx' -o \
+        -name '*.ts' -o \
+        -name '*.tsx' \
+      \) \
+      -print
+  ))
+fi
+
+./node_modules/.bin/prettier --write "${FILES[@]}"
+./node_modules/.bin/eslint --fix "${FILES[@]}"
