8c522ae359759dc9bab87d46aca9f8b0870420c9
Ian MacLeod <ian@nevir.net>
2018-03-03 21:50:12 -0800

    Test for style violations via prettier

--- template/scripts/test-style.sh
+++ template/scripts/test-style.sh
@@ -19,3 +19,18 @@ if [[ "${#FILES[@]}" = "0" ]]; then
 fi
 
 ./node_modules/.bin/eslint "${FILES[@]}"
+
+set +e
+UGLY_FILES=($(./node_modules/.bin/prettier --list-different "${FILES[@]}"))
+if [[ "${#UGLY_FILES[@]}" != "0" ]]; then
+  echo
+  echo -e "\033[4m\033[33mThe following files are not well formatted:\033[0m"
+  echo
+  for file in "${UGLY_FILES[@]}"; do
+    echo "  ${file}"
+  done
+  echo
+  echo -e "\033[31mPlease fix via: \033[33mnpm run fix-style\033[0m"
+  echo
+  exit 1
+fi
